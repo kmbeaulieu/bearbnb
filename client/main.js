d4faf2031f3ca0ab5e65fa860cc78ce35d3cbe5a
Krystle <krystleweinrich@yahoo.com>
2016-11-29 00:53:35 -0800

    main has a nav bar. can log in and log out

--- client/main.js
+++ client/main.js
@@ -1,22 +1,32 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
+import {Meteor} from 'meteor/meteor';
+import {FlowRouter} from 'meteor/kadira:flow-router';
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+    counter() {
+        return Template.instance().counter.get();
+    },
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    'click button'(event, instance) {
+        // increment the counter when button is clicked
+        instance.counter.set(instance.counter.get() + 1);
+    },
 });
+
+Template.navbar.events({
+    'click .logout': function (event) {
+        event.preventDefault();
+        Meteor.logout();
+    },
+});
+
