53a9d826f1956a3a687de65fb793a6ea256a8126
nameisyoung <nameisyoung@gmail.com>
2016-12-06 11:57:29 -0800

    Update main.js

--- client/main.js
+++ client/main.js
@@ -33,3 +33,25 @@ Template.navigationbar.events({
     }
 });
 let user = Meteor.userId();
+
+
+Template.loginBox.events({
+
+    'submit #login-form' : function(e, t){
+      e.preventDefault();
+      // retrieve the input field values
+      var email = t.find('#login-email').value,
+          password = t.find('#login-password').value;
+
+        Meteor.loginWithPassword(email, password, function(err){
+        if (err){
+          toastr.warning("Login Failed");
+        }
+        else{
+          toastr.success("Logged in");
+
+        }
+      });
+         return false;
+      }
+  });
